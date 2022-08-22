c3336164e8316b348d2a3c5728a3e11e3948fd24
段欣欣 <duanxinxin@58.com>
2022-08-22 23:20:14 +0800

    feat: 数据库

---
+++ src/common/filters/http-exception.filter.ts
@@ -0,0 +1,32 @@
+/*
+ * @Author: duanxinxin
+ * @Date: 2022-08-16 14:26:10
+ * @LastEditors: duanxinxin
+ * @LastEditTime: 2022-08-16 14:42:15
+ * @Description:
+ */
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from '@nestjs/common';
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter<HttpException> {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+    const status = exception.getStatus();
+    console.log(exception);
+    const exceptionRes: any = exception.getResponse();
+    const { error, message } = exceptionRes;
+    response.status(status).json({
+      status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      error,
+      message,
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,19 @@
  * @Author: duanxinxin
  * @Date: 2022-08-12 15:26:11
  * @LastEditors: duanxinxin
- * @LastEditTime: 2022-08-14 23:19:40
+ * @LastEditTime: 2022-08-16 15:03:55
  * @Description:
  */
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule);
 
   //设置swagger文档相关配置
   const swaggerOptions = new DocumentBuilder()
