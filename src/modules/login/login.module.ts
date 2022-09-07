27f3f38f01c704fc02ca8a56081681a2f3b5e4fb
段欣欣 <duanxinxin@58.com>
2022-09-07 15:16:10 +0800

    feat: loginMoudle

---
+++ src/modules/login/login.module.ts
@@ -0,0 +1,13 @@
+/*
+ * @Author: duanxinxin
+ * @Date: 2022-08-23 20:17:17
+ * @LastEditors: duanxinxin
+ * @LastEditTime: 2022-08-23 20:18:11
+ * @Description:
+ */
+import { Module } from '@nestjs/common';
+
+@Module({
+  controllers: [],
+})
+export class LoginModule {}
